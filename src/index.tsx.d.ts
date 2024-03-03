dbec23242013f76afca62bbf64edbe92284a58f2
George Khaburzania <giokhabu@gmail.com>
2024-03-03 16:29:58 +0400

    remove extra files and remove .env exception from gitignore

--- src/index.tsx.d.ts
+++
@@ -1,9 +0,0 @@
-declare global {
-    namespace ReactDOM {
-      function createRoot(container: Element | Document | DocumentFragment | null): Root;
-    }
-  }
-  
-  interface Root {
-    render(jsx: React.ReactNode): void;
-  }
